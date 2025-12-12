e5a603f4517f5a8f02ab6bc5eb64af384ea38016
Max <214122627+Diplomoceras-maximum@users.noreply.github.com>
2025-12-12 16:58:47 +0000

    Add examples of the fibonacci sequence generated both iteratively and recursively

--- script.js
+++ script.js
@@ -82,3 +82,65 @@ function fibonacci(n) {
 
 // Time complexity:
 // O(n log n)
+
+// ##################################################
+// Fibonacci sequence iteratively
+// ##################################################
+
+function fibsIte(n) {
+  // Return nothing if negative number
+  if (n < 0) {
+    return [];
+  }
+
+  // Return 0 if number is 0
+  if (n === 0) {
+    return [0];
+  }
+
+  // Return 0, 1 if number is 1
+  if (n === 1) {
+    return [0, 1];
+  }
+
+  // First two numbers
+  const sequence = [0, 1];
+
+  // Loop to give rest of sequence
+  for (let i = 2; i <= n; i++) {
+    const nextFib = sequence[i - 1] + sequence[i - 2];
+    sequence.push(nextFib);
+  }
+  // Return completed sequence
+  return sequence;
+}
+
+// ##################################################
+// Fibonacci sequence recursively
+// ##################################################
+
+function fibsRec(n) {
+  // Return nothing if negative number
+  if (n < 0) {
+    return [];
+  }
+
+  // Return 0 if number is 0
+  if (n === 0) {
+    return [0];
+  }
+
+  // Return 0, 1 if number is 1
+  if (n === 1) {
+    return [0, 1];
+  }
+
+  // Recursion
+  const array = fibsRec(n - 1);
+
+  // Get the next number in sequence and push to array
+  array.push(array[array.length - 1] + array[array.length - 2]);
+
+  // Return the full sequence
+  return array;
+}
