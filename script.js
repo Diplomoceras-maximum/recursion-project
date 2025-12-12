3004471c7802aa870b76c61054a23ea938b90e71
Max <214122627+Diplomoceras-maximum@users.noreply.github.com>
2025-12-12 16:39:17 +0000

    Add explanation and example of merge sort

--- script.js
+++ script.js
@@ -20,3 +20,65 @@ function fibonacci(n) {
 }
 
 // 0, 1, 1, 2, 3, 5
+
+// ##################################################
+// Merge Sort
+// ##################################################
+
+// In merge sort the idea of the algorithm is to sort smaller arrays
+// and then combine those arrays together (merge them) in sorted order.
+
+// Meaning:
+// 1. sort the left half of the array (if n > 1)
+// 2. sort the right half of the array (if n > 1)
+// 3. then merge the two halves together
+
+// Merge sort uses recursion.
+
+// Example of merge sort:
+
+// Array: 5, 2, 1, 3, 6, 4
+// sort left half of array
+// 5, 2, 1
+// sort left half of first array
+// (5), 2, 1
+// 5, 2, 1
+// sort right half of first array
+// 5, (2, 1)
+// 5, 2, 1
+// sort left side of first array right side
+// 5, 1, 2
+// sort right side of first array right side
+// 5, 1, 2
+// merge both halfs together
+// 1, 2, 5
+
+// sort right half of array
+// 3, 6, 4
+//sort left half of first array
+// (3), 6, 4
+// 3, 6, 4
+// sort right half of first array
+// 3, (6, 4)
+// 3, 6, 4
+// sort left side of first array right side
+// 3, 4, 6
+// sort right side of first array right side
+// 3, 4, 6
+// merge both halfs together
+// 3, 4, 6
+
+// Arrays: 1, 2, 5 | 3, 4, 6
+// merge arrays together
+// 1, 2, 3, 4, 5, 6
+
+// Worst-case scenario:
+// You have top split n elements up and then recombine them, effectively
+// doubling the sorted subarrays as they are built up.
+
+// Best-case scenario:
+// The array is already perfectly sorted, but it will still be split
+// up and recombined together by the algorithm
+
+// Time complexity:
+// O(n log n)
